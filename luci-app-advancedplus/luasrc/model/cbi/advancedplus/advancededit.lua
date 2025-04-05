6f0bb6c5d043592b04c34947123fecb4548f7583
YunHair <71691550+YunHair@users.noreply.github.com>
2025-04-05 21:37:00 +0800

    Update advancededit.lua

--- luci-app-advancedplus/luasrc/model/cbi/advancedplus/advancededit.lua
+++ luci-app-advancedplus/luasrc/model/cbi/advancedplus/advancededit.lua
@@ -53,6 +53,7 @@ CreateTab(s, "parentcontrol", "/etc/config/parentcontrol")
 CreateTab(s, "smartdns", "/etc/config/smartdns")
 CreateTab(s, "socat", "/etc/config/socat")
 CreateTab(s, "wolplus", "/etc/config/wolplus")
+CreateTab(s, "supervisor", "/etc/config/supervisor")
 
 CreateTab(s, "ddns-go", "/etc/ddns-go/ddns-go-config.yaml")
 
